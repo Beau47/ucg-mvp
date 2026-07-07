08f94ffe0c4825a11475df732db40f90ed9c31c1
Miranda Li <kejia@Mirandas-Macbook-Pro.local>
2026-07-07 14:21:58 -0500

    added an extra lesson for testing and fixed lesson appearance error

--- website/lessons.py
+++ website/lessons.py
@@ -1,4 +1,6 @@
-
+# =========================
+# Lesson definitions
+# =========================
 
 LESSONS = {
     "variables": {
@@ -57,6 +59,80 @@ print(x)"""
             {
                 "type": "exercise",
                 "problem": "add_one"
+            }
+
+        ]
+    },
+        "data_types": {
+
+        "id": "data_types",
+
+        "lesson_number": "Lesson 2",
+
+        "title": "Data Types",
+
+        "blocks": [
+
+            {
+                "type": "heading",
+                "text": "What are Data Types?"
+            },
+
+            {
+                "type": "paragraph",
+                "text":
+                "Data types describe the kind of information a variable stores. Python automatically determines the type of a value when it is assigned."
+            },
+
+            {
+                "type": "code",
+                "language": "python",
+                "text":
+"""name = "Alex"
+age = 15
+height = 5.8
+
+print(name)
+print(age)
+print(height)"""
+            },
+
+            {
+                "type": "paragraph",
+                "text":
+                "The variable name stores text (a string), age stores a whole number (an integer), and height stores a decimal number (a float)."
+            },
+
+            {
+                "type": "image",
+                "src": "/static/images/data_types.png",
+                "caption": "Python has different types of data for storing information."
+            },
+
+            {
+                "type": "tip",
+                "text":
+                "Use the type() function to check what type of value a variable contains."
+            },
+
+            {
+                "type": "code",
+                "language": "python",
+                "text":
+"""x = 10
+
+print(type(x))"""
+            },
+
+            {
+                "type": "warning",
+                "text":
+                "Remember that strings must be surrounded by quotes."
+            },
+
+            {
+                "type": "exercise",
+                "problem": "create_variables"
             }
 
         ]
