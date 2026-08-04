a48a3693e0862084ecac1fd713d7f2bbc9b2ac80
Beau47 <bambroise007@gmail.com>
2026-08-04 14:49:59 -0500

    Update supabase_client.py

--- website/supabase_client.py
+++ website/supabase_client.py
@@ -7,11 +7,13 @@ load_dotenv()
 
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_KEY = (
+    os.getenv("SUPABASE_KEY")
+    or os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+)
 
 
 supabase = create_client(
     SUPABASE_URL,
     SUPABASE_KEY
 )
-
